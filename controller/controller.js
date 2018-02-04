18b4635cdf08601ddfe6060b3769896471aeb597
cris-chun <cris_chun@163.com>
2018-02-04 20:40:31 +0800

    邮箱认证提交

--- controller/controller.js
+++ controller/controller.js
@@ -5,6 +5,7 @@
 
 var fd = require("formidable");
 var users = require("../db/users");
+var nodemailer = require("nodemailer")
 
 // 登陆业务
 exports.loginCheck = function(req,res,callback){
@@ -65,10 +66,56 @@ exports.registerCheck = function(req,res,callback) {
             throw err
             return
         }
-        users.insertData(fields,function(data){
+        var obj = {
+            username: fields.username,
+            password: fields.password,
+            phone_number: fields.phoneNumber,
+            sex: fields.sex,
+            check: false,
+            birthday: fields.birthday || '',
+            city: fields.city || '',
+            address: fields.address || '',
+            email: fields.email,
+            pwd_question: '',
+            pwd_answer: '',
+            avator: '',
+            tag: 0, // 0：普通用户   1：管理员
+            infos: 0,
+            follows: [],
+            posts: []
+        }
+        users.insertData(obj,function(data){
             if(data.result.ok == 1){
+                console.log("注册成功")
+                // 发送email
+                var href = 'http://localhost:3000/?username=' + fields.username + '&password=' +fields.password
+                // 发送方
+                var transporter = nodemailer.createTransport({
+                    service: 'qq',
+                    auth: {
+                        user: 'ztchun@qq.com',
+                        pass: 'bsdhtkkqjkbyhaia'
+                    }
+                })
+                // 接收方
+                var mailOptions = {
+                    from: 'ztchun@qq.com',
+                    to: fields.email,
+                    subject: '校园社区邮箱验证',
+                    text: '您好，此邮件为校园社区的邮箱认证，点击下方链接认证邮箱有效性',
+                    html: '<h2>您好，此邮件为校园社区的邮箱认证，请点击下方链接认证邮箱有效性</h2>' +
+                    '<div><a href="\' + href+ \'">链接</a></div>'
+                }
+                transporter.sendMail(mailOptions, function(error, info){
+                    if(error){
+                        console.log(error);
+                    }else{
+                        console.log('Message sent: ' + info.response);
+                    }
+                });
                 callback('1')
             } else {
+                console.log("注册失败")
                 callback('2')
             }
         })
